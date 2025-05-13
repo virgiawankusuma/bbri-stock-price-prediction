f2391f41bcf9c13cdb7489932ea182b5b92bcf7f
virgiawankusuma <virgiawan150@gmail.com>
2025-05-13 11:34:53 +0700

    fix: correct active class logic in main navigation template for proper highlighting

--- src/scripts/templates.js
+++ src/scripts/templates.js
@@ -14,7 +14,7 @@ export function generateMainNavigationListTemplate() {
   const activeRoute = window.location.hash.split('/')[1];
   const activeClass = (route) => (activeRoute === route ? 'active' : '');
   return `
-    <li class="nav-item"><a class="nav-link ${activeClass('')} " href="#/">Beranda</a></li>
+    <li class="nav-item"><a class="nav-link ${activeClass() || activeClass('')}" href="#/">Beranda</a></li>
     <li class="nav-item"><a class="nav-link ${activeClass('predict')}" href="#/predict">Prediksi Harga</a></li>
     <li class="nav-item"><a class="nav-link ${activeClass('about')}" href="#/about">Tentang</a></li>
   `;
